76dcced0fcfef83f79b6064eae4d156250bc55c8
AbyRin <875952018@qq.com>
2023-09-19 16:14:36 +0800

    20230919 update

---
+++ src/main/java/com/cpr/cprblog/controller/CommentController.java
@@ -0,0 +1,14 @@
+package com.cpr.cprblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CommentController {
+
+    @RequestMapping("/comment")
+    public String toComment() {
+
+        return "comment";
+    }
+}
---
+++ src/main/resources/static/js/comment.js
@@ -0,0 +1,11 @@
+// 获取元素
+const comment_top = document.querySelector('.top');
+
+// // 修改样式
+// comment_top.classList.add('mea');
+
+// // 切换样式
+// aqua.classList.toggle('mea');
+
+// // 修改文字内容
+// aqua.innerText = 'Mea';
