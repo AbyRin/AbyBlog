1e681fcec4beb4024fee1ffbe02a606428de7c58
AbyRin <cpr020620@outlook.com>
2023-09-20 17:02:36 +0800

    Add the comment function for the page Comment.html

--- src/main/resources/static/js/comment.js
+++ src/main/resources/static/js/comment.js
@@ -9,3 +9,22 @@ const comment_top = document.querySelector('.top');
 
 // // 修改文字内容
 // aqua.innerText = 'Mea';
+
+
+// 功能模块: 发帖  
+const tx = document.querySelector('#tx');
+const total = document.querySelector('.total');
+
+// 1. 文本域获得焦点时，让 total 显示
+tx.addEventListener('focus', function() {
+    total.style.opacity = 1;
+})
+// 2. 文本域失去焦点时，让 total 隐藏
+tx.addEventListener('blur', function() {
+    total.style.opacity = 0;
+})
+// 3. 检测用户输入
+tx.addEventListener('input', function() {
+    // console.log(tx.value.length);
+    total.innerHTML = `${tx.value.length}/200字`;
+})
