a2b24d8cc226ab3bb831bf013109881f20fc2dd0
AbyRin <cpr020620@outlook.com>
2023-09-23 01:24:56 +0800

    update the 3, 4 functions of the elevator

--- src/main/resources/static/js/public.js
+++ src/main/resources/static/js/public.js
@@ -10,12 +10,13 @@
     // 1. 卷去html头部 到达 商品搜索条-顶部坐标 时，就显示 电梯导航
     // 已改变方案
 
-    // 2. 卷去html头部 到达 商品搜索条-顶部坐标 时, 令 商品搜索条 变为fixed
+    // 2. 卷去html头部 到达 商品搜索条-顶部坐标 时, 令 商品搜索条 的top = 0
     window.addEventListener('scroll', function () {
-        if (document.documentElement.scrollTop >= search.offsetTop) {
-            search.style.position = 'sticky'
-        }
-        console.log(search.style.position)
+        // if (document.documentElement.scrollTop >= search.offsetTop) {
+        //     search.style.top = 0
+        // }
+        // console.log(search.style.position)
+        search.style.top = document.documentElement.scrollTop >= search.offsetTop ? 0 : '-100px'
     })
 
     // 功能二: 点击 Top: 页面顶部
@@ -69,10 +70,10 @@ window.addEventListener('scroll', function () {
 
     // 2. 判断页面当前滑动的位置, 选择小盒子
     // 获取分类class 和 页面滑动像素n
-    const class_1 = document.querySelector('.class_1')
-    const class_2 = document.querySelector('.class_2')
-    const class_3 = document.querySelector('.class_3')
-    const class_4 = document.querySelector('.class_4')
+    const class_1 = document.querySelector('.elevator_list li a[data-name = "class_1"]').dataset.name
+    const class_2 = document.querySelector('.elevator_list li a[data-name = "class_2"]').dataset.name
+    const class_3 = document.querySelector('.elevator_list li a[data-name = "class_3"]').dataset.name
+    const class_4 = document.querySelector('.elevator_list li a[data-name = "class_4"]').dataset.name
     const n = document.documentElement.scrollTop
 
     // 3. 匹配
